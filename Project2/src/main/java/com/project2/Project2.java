69b21dfe5feafa3b024292fd0699c230406c7242
musiczu <ravipol.cha2545@gmail.com>
2023-04-05 20:18:36 +0700

    Co-authored-by: Watcharasak Prommanee <watchanew@users.noreply.github.com>

---
+++ Project2/src/main/java/com/project2/Project2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+import java.io.*;
+import java.util.*;
+ 
+
+package com.project2;
+
+/**
+ *
+ * @author music
+ */
+public class Project2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(null);
+        System.out.println("Kuay music");
+        System.out.println("Enter number of rows square grid = ");
+        System.out.println("Enter initial states (4 bits, left to right, line by = ");
+        
+
+    }
+}
